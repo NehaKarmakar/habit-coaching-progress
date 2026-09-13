95aa280b7e68bb7274fb581e9a0b095808eb2058
NehaKarmakar <nehakarmakar2205@gmail.com>
2026-09-13 14:26:53 +0530

    updated backend

--- backend/utilis/sendEmail.js
+++ backend/utilis/sendEmail.js
@@ -2,7 +2,10 @@ import nodemailer from "nodemailer";
 import  dotenv  from "dotenv";
 dotenv.config()
 const transporter = nodemailer.createTransport( {
-    service: "gmail",
+    //service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.USER_PASSWORD
